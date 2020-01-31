7c6ba3c8099a2e0617d72a5072de8ba4d829a50f
derekli19951103 <derekli1995@outlook.com>
2020-01-31 01:33:46 -0500

    fix path

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 ﻿import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as send from "koa-send";
-import * as path from "path";
+import * as serve from "koa-static";
 
 const app = new Koa();
+app.use(serve(__dirname + "/client/build"));
 
 const router = new Router();
 router.get("/*", async ctx => {
-  await send(ctx, path.join(__dirname, "client", "build", "index.html"));
+  await send(ctx, "build/index.html", {
+    root: __dirname + "/client"
+  });
 });
 app.use(router.routes());
 
