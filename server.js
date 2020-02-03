4a6b79f039b49d2200a87e9b45a87f21c50c4382
derekli19951103 <derekli1995@outlook.com>
2020-02-03 16:49:29 -0500

    merge head add server

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+// start server
+const port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Server listening on port " + port);
+});
