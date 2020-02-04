e5824adafbcb0e03e7fbd914d61bcbe5902d3f7d
derekli19951103 <derekli1995@outlook.com>
2020-02-04 11:46:52 -0500

    change export

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import { Landing } from "./pages/Landing";
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return <Landing />;
 };
-
-export default App;
