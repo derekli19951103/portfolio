e8c2099cf32f76dcd66b563bafb676b0e98d8241
derekli19951103 <derekli1995@outlook.com>
2020-02-04 12:08:46 -0500

    build directly

--- README.md
+++ README.md
@@ -42,3 +42,6 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+
+"heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --dev --prefix client && npm run build --prefix client"
---
+++ client/build/precache-manifest.6014b9b27acd8567c23f1ce07b03fb79.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae7c07ea8982e2bbe858ec92fcbf3d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a2946854192277680a",
+    "url": "/static/css/2.e51c424c.chunk.css"
+  },
+  {
+    "revision": "6437f83c16c6faee1aab",
+    "url": "/static/css/main.be1a0da3.chunk.css"
+  },
+  {
+    "revision": "09a2946854192277680a",
+    "url": "/static/js/2.51c704ed.chunk.js"
+  },
+  {
+    "revision": "6437f83c16c6faee1aab",
+    "url": "/static/js/main.9caafd04.chunk.js"
+  },
+  {
+    "revision": "af5b5d48da1303eee455",
+    "url": "/static/js/runtime-main.a1327ed0.js"
+  },
+  {
+    "revision": "5629cb7c9948e526d63ccd6064a85ff9",
+    "url": "/static/media/DOF.5629cb7c.jpg"
+  },
+  {
+    "revision": "0f9d6950017058486d9475271cade77a",
+    "url": "/static/media/WISE.0f9d6950.jpg"
+  },
+  {
+    "revision": "0d2efa60bda9aae36b559a47363c543f",
+    "url": "/static/media/WebGL.0d2efa60.jpg"
+  },
+  {
+    "revision": "8fa77352da478f56c8edabd499d1cb99",
+    "url": "/static/media/antialiasing.8fa77352.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "5dd000317a6768fa7c7b519f0e1aa34c",
+    "url": "/static/media/card.5dd00031.jpg"
+  },
+  {
+    "revision": "441b928114c01ff9c1f57d5ae303d0e1",
+    "url": "/static/media/cel.441b9281.jpg"
+  },
+  {
+    "revision": "0bb9f69e224858fbadcf876b9f5cd8a6",
+    "url": "/static/media/cropped.0bb9f69e.jpg"
+  },
+  {
+    "revision": "abb45a85434c90d3cdb19db78628caea",
+    "url": "/static/media/deskewed.abb45a85.jpg"
+  },
+  {
+    "revision": "e7c62807b3f0742ed9c8623e6a2253ad",
+    "url": "/static/media/environmentmap.e7c62807.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0498f487d27c61f10f1b786cee34ec7b",
+    "url": "/static/media/glossy.0498f487.jpg"
+  },
+  {
+    "revision": "0f837fd4f21aedfb9d9498cf4d721dd8",
+    "url": "/static/media/gouraud.0f837fd4.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "3a0a1dee85aa1c28a7d1d9327f254fe3",
+    "url": "/static/media/origin.3a0a1dee.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "72f502e9d6165f44bd5e317eba8de57b",
+    "url": "/static/media/output.72f502e9.jpg"
+  },
+  {
+    "revision": "fa71215b95924347c4869edde9486afd",
+    "url": "/static/media/phong.fa71215b.jpg"
+  },
+  {
+    "revision": "63ab0c8255d7b97b56ebe18c69a57059",
+    "url": "/static/media/processed.63ab0c82.jpg"
+  },
+  {
+    "revision": "46b5ca8a3c78ed68f41a9cf75333be84",
+    "url": "/static/media/profilepic.46b5ca8a.jpg"
+  },
+  {
+    "revision": "5b629a3a2b0c26a51838f03a0a02e653",
+    "url": "/static/media/raytrace.5b629a3a.jpeg"
+  },
+  {
+    "revision": "87bf2d7424c7a74a932fcd69e9f78005",
+    "url": "/static/media/reflection.87bf2d74.jpg"
+  },
+  {
+    "revision": "6591ec79ad9f456993d74571907ad423",
+    "url": "/static/media/softshadow.6591ec79.jpg"
+  },
+  {
+    "revision": "bfe05cc0e838cb947f6c50cf19061206",
+    "url": "/static/media/texture.bfe05cc0.jpg"
+  },
+  {
+    "revision": "ecc692f398b44ff7896d2e257882187b",
+    "url": "/static/media/triangular.ecc692f3.jpg"
+  },
+  {
+    "revision": "409356869213d31e04afb5ceb8173070",
+    "url": "/static/media/wiseanimated.40935686.gif"
+  },
+  {
+    "revision": "3c86496dc94cf9847cfedbae1799c611",
+    "url": "/static/media/wisedownload.3c86496d.jpg"
+  },
+  {
+    "revision": "1199b98b212584598a56b1905da8e920",
+    "url": "/static/media/wisemobile.1199b98b.jpg"
+  }
+]);
